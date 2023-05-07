76d7aa5c2850690c8aa851e5f694f8c603acd16d
Joshua Ashkinaze <josh.ashkinaze@gmail.com>
2023-05-07 11:24:57 -0400

    add tenacity backoff if no table found error

--- scripts/manage_database/seed_db_with_human_responses.py
+++ scripts/manage_database/seed_db_with_human_responses.py
@@ -1,11 +1,3 @@
-"""
-Description: This script seeds the database with human responses to the AUT items
-
-Author: Joshua Ashkinaze
-
-Date: 05-05-2023
-"""
-
 import csv
 import uuid
 import logging
@@ -15,13 +7,18 @@ from google.oauth2 import service_account
 import os
 import time
 
+import tenacity
+from tenacity import retry, wait_fixed
 
+
+logging.basicConfig(filename=f'{os.path.basename(__file__)}.log', level=logging.INFO,
+                    format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S', filemode='w')
+
+class TableNotFoundError(Exception):
+    pass
+
+@retry(wait=wait_fixed(300), stop=tenacity.stop_after_attempt(12), retry_error_callback=lambda x: logging.info(x))
 def seed_database():
-    # Set up logging
-    logging.basicConfig(filename=f'{os.path.basename(__file__)}.log', level=logging.INFO,
-                        format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S',
-                        filemode='w')
-
     key_path = "../../creds/netcreate-0335ce05e7ff.json"
     credentials = service_account.Credentials.from_service_account_file(
         key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"],
@@ -33,6 +30,7 @@ def seed_database():
 
     # Read the CSV file and insert the rows into the BigQuery table
     with open("../../data/seed_human_responses.csv", newline="") as csvfile:
+        logging.info("Trying to seed human responses")
         reader = csv.DictReader(csvfile)
         response_date = datetime(2022, 1, 1)
 
@@ -50,11 +48,19 @@ def seed_database():
             }
 
             # Insert the row into the BigQuery table
-            errors = client.insert_rows_json(table, [bq_row])
-            if not errors:
-                logging.info(f"Row has been added: {bq_row}")
-            else:
-                logging.error(f"Encountered errors while inserting rows: {errors}")
+            try:
+                errors = client.insert_rows_json(table, [bq_row])
+                if not errors:
+                    logging.info(f"Row has been added: {bq_row}")
+                else:
+                    logging.error(f"Encountered errors while inserting rows: {errors}")
+            except Exception as e:
+                error_msg = str(e)
+                logging.info(f"Encountered exception: {error_msg}. Will sleep for 5 minutes and retry.")
+                if "Table" in error_msg and "not found" in error_msg:
+                    raise TableNotFoundError(error_msg)
+                else:
+                    raise e
 
             # Increment the response_date by 1 minute
             response_date += timedelta(minutes=1)
