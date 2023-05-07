37fc565d1117e5158cd4b65c14bda5336294aad8
Joshua Ashkinaze <josh.ashkinaze@gmail.com>
2023-05-07 08:21:05 -0400

    improve table clearing

--- scripts/manage_database/clear_table.py
+++ scripts/manage_database/clear_table.py
@@ -4,32 +4,27 @@ from google.oauth2 import service_account
 import os
 
 # Set up logging
+LOG_FORMAT = '%(asctime)s %(levelname)s: %(message)s'
 logging.basicConfig(filename=f'{os.path.basename(__file__)}.log', level=logging.INFO,
-                    format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+                    format=LOG_FORMAT, datefmt='%Y-%m-%d %H:%M:%S', filemode='w')
 
-def main():
+def truncate_table(credentials, dataset_id, table_id):
+    client = bigquery.Client(credentials=credentials, project=credentials.project_id)
+
+    # Delete all rows from the table
+    table_ref = client.dataset(dataset_id).table(table_id)
+    delete_query = f"DELETE FROM `{table_ref.project}.{table_ref.dataset_id}.{table_ref.table_id}` WHERE 1=1"
+    query_job = client.query(delete_query)
+    query_job.result()
+
+    logging.info(f"All rows truncated from {dataset_id}.{table_id}")
+
+if __name__ == "__main__":
     key_path = "../../creds/netcreate-0335ce05e7ff.json"
     credentials = service_account.Credentials.from_service_account_file(
         key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"],
     )
 
-    client = bigquery.Client(credentials=credentials, project=credentials.project_id)
     dataset_id = "net_expr"
     table_id = "trials"
-
-    # Get the schema of the existing table
-    table_ref = bigquery.DatasetReference(credentials.project_id, dataset_id).table(table_id)
-    table = client.get_table(table_ref)
-    schema = table.schema
-
-    # Delete the table
-    client.delete_table(table_ref)
-
-    # Create a new empty table with the same schema
-    new_table = bigquery.Table(table_ref, schema=schema)
-    client.create_table(new_table)
-
-    logging.info(f"All rows cleared from {dataset_id}.{table_id}")
-
-if __name__ == "__main__":
-    main()
+    truncate_table(credentials, dataset_id, table_id)
