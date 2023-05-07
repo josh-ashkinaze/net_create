64e124d5541bf9b0526d8aebca55e824ad811609
Joshua Ashkinaze <josh.ashkinaze@gmail.com>
2023-05-07 17:55:21 -0400

    clean up seeding human script

--- scripts/manage_database/get_human_seed_responses.py
+++ scripts/manage_database/get_human_seed_responses.py
@@ -1,10 +1,3 @@
-"""
-Description: This script seeds the database with human responses to the AUT items
-
-Author: Joshua Ashkinaze
-
-Date: 05-05-2023
-"""
 import pandas as pd
 import random
 import logging
@@ -17,50 +10,44 @@ def main():
 
     my_items = ["box", "fork", "lightbulb", "spoon", "table"]
 
-    # Number of human responses needed:
-    # H = 6
-    # F, L: 2
-    # F, U: 2
-    # M, L: 4
-    # M, U: 4
-    n_items_per_condition = [6, 2, 2, 4, 4]
+    n_items_per_condition = {
+        "h": 6,
+        "f_l": 2,
+        "f_u": 2,
+        "m_l": 4,
+        "m_u": 4
+    }
 
     # Read in the data and filter to only include the relevant prompts
     df = pd.read_csv("../../data/prior_responses.csv")
     df = df[df['prompt'].isin(my_items)]
-    df = df.sample(frac=1, random_state=417)
 
     # Initialize a list to hold the assignments
     assignments = []
 
-    # Loop over each prompt
     for prompt in my_items:
-        # Get a list of all responses for this prompt
-        responses = df[df['prompt'] == prompt]['response'].unique().tolist()
-        scores = df[df['prompt'] == prompt]['target'].unique().tolist()
+        # Filter data for current prompt
+        prompt_df = df[df['prompt'] == prompt]
 
-        for i, n_items in enumerate(n_items_per_condition):
-            condition = ""
-            if i == 0:
-                condition = "h"
-            elif i == 1:
-                condition = "f_l"
-            elif i == 2:
-                condition = "f_u"
-            elif i == 3:
-                condition = "m_l"
-            else:
-                condition = "m_u"
-            for j in range(n_items):
-                response = responses.pop(0)
-                score = scores.pop(0)
-                assignments.append({'item': prompt, 'response': response.lower(), 'score':score, 'condition': condition})
+        for condition, n_items in n_items_per_condition.items():
+            # Sample n_items from prompt_df for each condition
+            sampled_df = prompt_df.sample(n_items, random_state=417)
 
-    # Convert the list of dictionaries to a DataFrame
-    assignments_df = pd.DataFrame(assignments)
-    assignments_df.to_csv("../../data/seed_human_responses.csv")
+            # Remove sampled rows from prompt_df
+            prompt_df = prompt_df.drop(sampled_df.index)
+
+            # Add condition column to sampled_df
+            sampled_df['condition'] = condition
+
+            # Append sampled_df to assignments
+            assignments.append(sampled_df)
+
+    # Concatenate all the DataFrames in assignments
+    assignments_df = pd.concat(assignments)
+
+    # Save the result
+    assignments_df.to_csv("../../data/seed_human_responses.csv", index=False)
     logging.info("All good, got the seed human responses.")
-
 
     logging.info(f"Created human_responses.csv with {len(assignments_df)} rows.")
 
