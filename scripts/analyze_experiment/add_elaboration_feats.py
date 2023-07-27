71e3653eee3ff4416a007f39adabd675e0a5c543
Joshua Ashkinaze <josh.ashkinaze@gmail.com>
2023-07-27 14:52:15 -0400

    add logging

--- scripts/analyze_experiment/add_elaboration_feats.py
+++ scripts/analyze_experiment/add_elaboration_feats.py
@@ -3,6 +3,7 @@ import numpy as np
 import spacy
 import os
 import requests
+import logging
 import bz2
 from spacy.lang.en.stop_words import STOP_WORDS
 from joblib import Parallel, delayed
@@ -33,19 +34,26 @@ def process_text(tfidf_filtered, tokens, stopwords):
 
 
 if __name__ == "__main__":
+    LOG_FORMAT = '%(asctime)s %(levelname)s: %(message)s'
+    logging.basicConfig(filename=f'{os.path.basename(__file__)}.log', level=logging.INFO, format=LOG_FORMAT,
+                        datefmt='%Y-%m-%d %H:%M:%S', filemode='w')
     get_tfidf_file()
     tfidf = pd.read_csv("../../data/experiment_data/tfidf_weights.csv")
     df = pd.read_csv("../../data/experiment_data/data_clean.csv")
+    logging.info("length of df before dropping na: " + str(len(df)))
     df = df.dropna(subset=['response_text'])
+    logging.info("length of df after dropping na: " + str(len(df)))
 
     nlp = spacy.load("en_core_web_sm", disable=["tagger", "parser", "ner"])
 
     # get all unique tokens in one go
     all_docs = list(nlp.pipe(df['response_text'], batch_size=500))
     all_tokens = {token.text for doc in all_docs for token in doc if token.is_alpha}
+    logging.info("All tokens extracted")
 
     # filter tfidf once for all tokens
     tfidf_filtered = tfidf[tfidf['token'].isin(all_tokens)]
+    logging.info("TFIDF filtered")
 
     results = Parallel(n_jobs=-1)(
         delayed(process_text)(tfidf_filtered, [token.text for token in doc if token.is_alpha], STOP_WORDS) for doc in
@@ -53,4 +61,7 @@ if __name__ == "__main__":
 
 
     df['elab_ibf'], df['elab_ipf'], df['elab_not_sw'], df['elab_n_tokens'] = zip(*results)
+    logging.info("length of df" + str(len(df)))
+    logging.info("Elaboration features added")
     df.to_csv("../../data/experiment_data/data_clean_with_elab.csv", index=False)
+    logging.info("Data saved")
