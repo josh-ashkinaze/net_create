81f18d541f21fbf6221441b97c03e1332dde663c
Joshua Ashkinaze <josh.ashkinaze@gmail.com>
2023-07-28 22:37:32 -0400

    add cols for ideas, dont delete excluded ones

--- scripts/analyze_experiment/pull_and_process_data.sh
+++ scripts/analyze_experiment/pull_and_process_data.sh
@@ -5,7 +5,7 @@ NOW=$(date +"%Y_%m_%d_%H%M%S")
 LOGFILE="${NOW}_data_process_pipeline.log"
 
 echo "Pulling data from the database" | tee -a $LOGFILE
-jupyter nbconvert --execute get_data.ipynb >> $LOGFILE 2>&1
+papermill get_data.ipynb get_data.ipynb
 
 echo "Adding elaboration features" | tee -a $LOGFILE
 python3 add_elaboration_feats.py >> $LOGFILE 2>&1
