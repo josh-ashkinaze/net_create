64c564b49dd4c761a4858bc90dbd994cd98dc6f8
Joshua Ashkinaze <josh.ashkinaze@gmail.com>
2023-08-24 14:53:05 -0400

    add rmd

--- scripts/analyze_experiment/run_pipeline.sh
+++ scripts/analyze_experiment/run_pipeline.sh
@@ -10,7 +10,13 @@ papermill get_data.ipynb get_data.ipynb
 echo "Adding elaboration features" | tee -a $LOGFILE
 python3 add_elaboration_feats.py >> $LOGFILE 2>&1
 
+echo "Adding originality features" | tee -a $LOGFILE
+#python3 add_originality_feats.py >> $LOGFILE 2>&1
+
 echo "Adding diversity analysis" | tee -a $LOGFILE
 papermill diversity_metrics.ipynb diversity_metrics.ipynb
+
+echo "Running R notebook"
+Rscript -e "rmarkdown::render('r_models.Rmd', output_file='r_models.html')" >> $LOGFILE 2>&1
 
 echo "Done" | tee -a $LOGFILE
